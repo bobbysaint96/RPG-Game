a9cb3b69c54328964a211ac6e1daabea432133ed
bobbysaint96 <bobbysaint96@gmail.com>
2019-03-01 15:13:13 -0500

    Initial Commit
    Whiteboxed:
    -Home Screen
    -Battle Scene
    -Map Layout

---
+++ RPG/Assets/UnitStats.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+
+    public float health;
+    public float mana;
+    public float attack;
+    public float magic;
+    public float defense;
+    public float speed;
+
+}
